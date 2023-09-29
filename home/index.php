315056c92d58ee6a35830d2f161cdf329a9ad5b0
Ngoc <nt7nguyen@gmail.com>
2023-09-29 09:14:02 +0200

    first push
    pushing the homepage item spotlights

--- home/index.php
+++ home/index.php
@@ -1,11 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NerdyGadgets | home</title>
 </head>
 <body>
-    <h1>hello world</h1>
+    <h3>Hello world</h3>
+    
+    <div id="homepage-container">
+        <div>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="homepagebanner"></a>
+        </div>
+    </div>
+
+    <div id="homepage-container">
+        <div class="row">
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+        </div>
+    </div>
+
+    <div id="homepage-container">
+        <div class="row">
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+        </div>
+    </div>
+
+    <div id="homepage-container">
+        <div>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productbanner"></a>
+        </div>
+    </div>
+
+    <div id="homepage-container">
+        <div class="row">
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            <a href="https://google.com"><img src="img/highlighted-products/headphones.jpg" alt="Headphones" class="productboxes"></a>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
