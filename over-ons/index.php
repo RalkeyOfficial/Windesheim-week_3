8458ed4288bbdf461d614c1280b619dbe40f3d15
Ralkey <Ralkey@outlook.com>
2023-10-01 19:41:02 +0200

    Update index.php

--- over-ons/index.php
+++ over-ons/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="overstyle2.css">
+    <link rel="stylesheet" href="./overstyle2.css">
     <title>Over Ons - Nerdy Gadgets</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
