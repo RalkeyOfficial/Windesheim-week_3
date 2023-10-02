2f5a95dcf983bc1d1de78778c99cedb456cb71e7
Catzu <nt7nguyen@gmail.com>
2023-10-02 21:17:08 +0200

    example
    added test code to show my teammate how this worked

---
+++ home/test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="css/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="center">
+        <a href="https://google.com"><div style="height: 200px; width: 200px; background-color: blue;">test</div></a>
+        <div style="height: 200px; width: 200px; background-color: red;">test</div>
+        <div style="height: 200px; width: 200px; background-color: yellow;">test</div>
+        <div style="height: 200px; width: 200px; background-color: green;">test</div> 
+    </div>
+    <div class="">
+        <div style="height: 200px; width: 200px; background-color: blue;">test</div>
+        <div style="height: 200px; width: 200px; background-color: red;">test</div>
+        <div style="height: 200px; width: 200px; background-color: yellow;">test</div>
+        <div style="height: 200px; width: 200px; background-color: green;">test</div> 
+    </div>
+</body>
+</html>
