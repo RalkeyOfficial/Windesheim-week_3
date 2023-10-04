5a32eeda320876f86fe74d6a7581e3d0ff77998d
Ralkey <s1199867@student.windesheim.nl>
2023-10-04 11:49:01 +0200

    small changes

--- components/header.php
+++ components/header.php
@@ -2,16 +2,21 @@
     <?php include_once ROOT.'/components/header.css' ?>
 </style>
 
+<script src="https://kit.fontawesome.com/ee85149100.js" crossorigin="anonymous"></script>
+
 <header>
     <div class="head">
-        <div class="logo">
+        <a href="../../../../../../home/" class="logo">
             <img src="../../../../../../images/Logo-website.png" alt="">
             <h1>Nerdy Gadgets</h1>
-        </div>
+        </a>
         <div class="search">
+            <input type="text">
+            <button><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
+        </div>
+        <div class="buttons">
             
         </div>
-        <div class="buttons"></div>
     </div>
     <div class="seperator" />
     <div class="navbar">
