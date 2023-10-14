f961ab981858fac24a4fff57fe0dc5d548b3f66b
Ngoc <nt7nguyen@gmail.com>
2023-10-14 20:57:42 +0200

    Recenties gerealiseerd
    lukt me nogsteeds niet om de gerelateerde producten goed in het scherm te krijgen. Vanwege dit heb ik de father div-wrap gecomment

--- Productpagina/index.php
+++ Productpagina/index.php
@@ -37,70 +37,143 @@
     </div>
 </div>
 
+<!-- <div class="wrap"> -->
+    <!------Product beoordeling------>
+    <div class="p-container">
+        <h2 class="title">Recenties</h2>
+        <div class="box">
+            <div class="review">
+                <div class="evenly">
+                    <div class="klantreviews">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <h5>14/10/2023</h5>
+                </div>
+                <h2>Product titel</h2>
+                <h4>Klantnaam</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.</p>
+                <div class="behulpzaam">
+                    <div>Behulpzaam?</div>
+                    <div>Ja(0)</div>
+                    <div>Nee(0)</div>
+                </div>
+            </div>
+        </div>
 
-<!------gerelateerde producten------>
-<div class="p-container">
-    <h2 class="title">Gerelateerde producten</h2>
-    <div class="row">
-        <div class="col-4">
-            <a href="../Productpagina/">
-                <img src="images/asus1.jpg" alt="product">
-            </a>
-            <h4>Asus Tuf Gaming A15</h4>
-            <div class="klantreviews">
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
+        <div class="box">
+            <div class="review">
+                <div class="evenly">
+                    <div class="klantreviews"> 
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <h5>14/10/2023</h5>
+                </div>
+                <h2>Product titel</h2>
+                <h4>Klantnaam</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.</p>
+                <div class="behulpzaam">
+                    <div>Behulpzaam?</div>
+                    <div>Ja(0)</div>
+                    <div>Nee(0)</div>
+                </div>
             </div>
-            <p>€1050.00</p>
         </div>
-        <div class="col-4">
-            <a href="../Productpagina/">
-                <img src="images/asus2.jpg" alt="product">
-            </a>
-            <h4>ASUS X415EA-EB1510W</h4>
-            <div class="klantreviews">
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star-o"></i>
+
+        <div class="box">
+            <div class="review">
+                <div class="evenly">
+                    <div class="klantreviews"> 
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                    </div>
+                    <h5>14/10/2023</h5>
+                </div>
+                <h2>Product titel</h2>
+                <h4>Klantnaam</h4>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type.</p>
+                <div class="behulpzaam">
+                    <div>Behulpzaam?</div>
+                    <div>Ja(0)</div>
+                    <div>Nee(0)</div>
+                </div>
             </div>
-            <p>€700.00</p>
-        </div>
-        <div class="col-4">
-            <a href="../Productpagina/">
-                <img src="images/asus3.jpg" alt="product">
-            </a>
-            <h4>ASUS Vivobook 15</h4>
-            <div class="klantreviews">
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star-half-o"></i>
-                <i class="fa fa-star-o"></i>
-            </div>
-            <p>€800.00</p>
-        </div>
-        <div class="col-4">
-            <a href="../Productpagina/">
-                <img src="images/asus4.jpg" alt="product">
-            </a>
-            <h4>ASUS Vivobook 16</h4>
-            <div class="klantreviews">
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star"></i>
-                <i class="fa fa-star-o"></i>
-                <i class="fa fa-star-o"></i>
-            </div>
-            <p>€850.00</p>
         </div>
     </div>
-</div>
 
+    <!------gerelateerde producten------>
+    <div class="p-container">
+        <h2 class="title">Gerelateerde producten</h2>
+        <div class="row">
+            <div class="col-4">
+                <a href="../Productpagina/">
+                    <img src="images/asus1.jpg" alt="product">
+                </a>
+                <h4>Asus Tuf Gaming A15</h4>
+                <div class="klantreviews">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+                <p>€1050.00</p>
+            </div>
+            <div class="col-4">
+                <a href="../Productpagina/">
+                    <img src="images/asus2.jpg" alt="product">
+                </a>
+                <h4>ASUS X415EA-EB1510W</h4>
+                <div class="klantreviews">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>€700.00</p>
+            </div>
+            <div class="col-4">
+                <a href="../Productpagina/">
+                    <img src="images/asus3.jpg" alt="product">
+                </a>
+                <h4>ASUS Vivobook 15</h4>
+                <div class="klantreviews">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>€800.00</p>
+            </div>
+            <div class="col-4">
+                <a href="../Productpagina/">
+                    <img src="images/asus4.jpg" alt="product">
+                </a>
+                <h4>ASUS Vivobook 16</h4>
+                <div class="klantreviews">
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+                <p>€850.00</p>
+            </div>
+        </div>
+    </div>
+<!-- </div> -->
 
 <!-- footer -->
 <?php include_once ROOT . '/components/footer.php' ?>
