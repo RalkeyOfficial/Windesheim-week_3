84a4751b32c05c5080a25e6377c4c58fb9a2fe01
Ngoc <nt7nguyen@gmail.com>
2023-10-17 22:39:16 +0200

    Finalized the productpage
    Finished the figma design with the reworked changes.
    connected to db.
    Imported info from db into the productpage.

---
+++ api/product-gerelateerde.php
@@ -0,0 +1,18 @@
+<?php 
+include_once __DIR__ . "\db\dbc.php";
+
+// SIMPLE GET REQUEST
+
+// define query
+
+$query = "
+SELECT * 
+FROM product
+LIMIT 4;
+";
+
+// execute query & get result
+// WARNING: you will only use query() if no user defined data will be used
+// if ANYTHING in this query you receive from the user use a prepared statement
+
+$result = $conn->query($query);
