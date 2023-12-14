aed9bcb23b44599427c55ca365de3d4fc62cbf01
SYLCN05 <Siyaryalcin25@gmail.com>
2023-12-14 12:51:26 +0100

    update review login easteregg

--- inlog/login.js
+++ inlog/login.js
@@ -1,4 +1,3 @@
-
 loginButton = document.getElementById('loginButton');
 var originalTransform = window.getComputedStyle(loginButton).getPropertyValue('transform');
 
@@ -6,10 +5,35 @@ loginButton.addEventListener('mouseenter', function () {
     var emailValue = document.getElementById('email').value;
     var passwordValue = document.getElementById('password').value;
 
-    if (emailValue.trim() === '' || passwordValue.trim() === '') {
+
+    if (emailValue.trim() === '') {
         loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
         loginButton.setAttribute('disabled', 'true');
+        return;
     }
+
+
+    var emailRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
+    if (!emailRegex.test(emailValue)) {
+        loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
+        loginButton.setAttribute('disabled', 'true');
+        return;
+    }
+
+
+    if (passwordValue.trim() === '') {
+        loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
+        loginButton.setAttribute('disabled', 'true');
+        return;
+    }
+
+
+    if (passwordValue.length < 8) {
+        loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
+        loginButton.setAttribute('disabled', 'true');
+        return;
+    }
+
 });
 
 document.addEventListener('input', function () {
