5750d95e0949dcf5153cae62d5582229c743e2b3
Ralkey <Ralkey@outlook.com>
2023-12-14 14:42:20 +0100

    Update login.js

--- inlog/login.js
+++ inlog/login.js
@@ -1,47 +1,52 @@
-loginButton = document.getElementById('loginButton');
-var originalTransform = window.getComputedStyle(loginButton).getPropertyValue('transform');
+loginButton = document.getElementById("loginButton");
+var originalTransform = window.getComputedStyle(loginButton).getPropertyValue("transform");
 
-loginButton.addEventListener('mouseenter', function () {
-    var emailValue = document.getElementById('email').value;
-    var passwordValue = document.getElementById('password').value;
+const emailRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
 
+loginButton.addEventListener("mouseenter", function () {
+    var emailValue = document.getElementById("email").value;
+    var passwordValue = document.getElementById("password").value;
 
-    if (emailValue.trim() === '') {
-        loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
-        loginButton.setAttribute('disabled', 'true');
+    if (emailValue.trim() === "") {
+        loginButton.style.transform =
+            "translate(" + Math.random() * 400 + "px, " + Math.random() * 300 + "px)";
+        loginButton.setAttribute("disabled", "true");
         return;
     }
 
-
-    var emailRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
     if (!emailRegex.test(emailValue)) {
-        loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
-        loginButton.setAttribute('disabled', 'true');
+        loginButton.style.transform =
+            "translate(" + Math.random() * 400 + "px, " + Math.random() * 300 + "px)";
+        loginButton.setAttribute("disabled", "true");
         return;
     }
 
-
-    if (passwordValue.trim() === '') {
-        loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
-        loginButton.setAttribute('disabled', 'true');
+    if (passwordValue.trim() === "") {
+        loginButton.style.transform =
+            "translate(" + Math.random() * 400 + "px, " + Math.random() * 300 + "px)";
+        loginButton.setAttribute("disabled", "true");
         return;
     }
-
 
     if (passwordValue.length < 8) {
-        loginButton.style.transform = 'translate(' + Math.random() * 400 + 'px, ' + Math.random() * 300 + 'px)';
-        loginButton.setAttribute('disabled', 'true');
+        loginButton.style.transform =
+            "translate(" + Math.random() * 400 + "px, " + Math.random() * 300 + "px)";
+        loginButton.setAttribute("disabled", "true");
         return;
     }
-
 });
 
-document.addEventListener('input', function () {
-    var emailValue = document.getElementById('email').value;
-    var passwordValue = document.getElementById('password').value;
+document.addEventListener("input", function () {
+    var emailValue = document.getElementById("email").value;
+    var passwordValue = document.getElementById("password").value;
 
-    if (emailValue.trim() !== '' && passwordValue.trim() !== '') {
+    if (
+        emailValue.trim() !== "" &&
+        passwordValue.trim() !== "" &&
+        passwordValue.length >= 8 &&
+        emailRegex.test(emailValue)
+    ) {
         loginButton.style.transform = originalTransform;
-        loginButton.removeAttribute('disabled');
+        loginButton.removeAttribute("disabled");
     }
 });
